47a52bf28e4825c882efd4baf0ce9ce431cf0cb9
赵新波 <zhaoxinbo@cm-inv.com>
2017-06-22 18:26:55 +0800

    增加了PriorityBlockingQueue DelayQueuelearn

---
+++ src/main/java/com/aboruo/constructor/queuelearn/DelayQueueLearn.java
@@ -0,0 +1,15 @@
+package com.aboruo.constructor.queuelearn;
+
+public class DelayQueueLearn implements Runnable {
+
+	@Override
+	public void run() {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/com/aboruo/constructor/queuelearn/QueueLearn.java
+++ src/main/java/com/aboruo/constructor/queuelearn/QueueLearn.java
@@ -3,6 +3,8 @@ package com.aboruo.constructor.queuelearn;
 import java.util.Iterator;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class QueueLearn {
@@ -24,6 +26,44 @@ public class QueueLearn {
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+		}
+		
+		/** 2 LinkedBlockingQueue 测试 */
+		BlockingQueue<String> blockingQueue = new LinkedBlockingQueue<String>(5);
+		blockingQueue.add("a");
+		blockingQueue.add("b");
+		blockingQueue.add("c");
+		blockingQueue.add("d");
+		blockingQueue.add("e");
+		/*try {
+//			blockingQueue.offer("f", 3, TimeUnit.SECONDS);
+//			blockingQueue.add("g");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+		System.out.println("linkedBlockingQueue.size ===" + blockingQueue.size());
+		for (Iterator<String> iterator = blockingQueue.iterator(); iterator.hasNext();) {
+			String string = (String) iterator.next();
+			System.out.println(string);
+		}
+		
+		/** 3. priorityBlockingQueue */
+		PriorityBlockingQueue<QueueElement> blockingQueue2 = new PriorityBlockingQueue<QueueElement>();
+		QueueElement queueElement1 = new QueueElement(1, "队列元素1");
+		QueueElement queueElement2 = new QueueElement(6, "队列元素2");
+		QueueElement queueElement3 = new QueueElement(3, "队列元素3");
+		blockingQueue2.add(queueElement1);
+		blockingQueue2.add(queueElement2);
+		blockingQueue2.add(queueElement3);
+		
+		while(blockingQueue2.size() > 0){
+			try {
+				QueueElement queueElement = blockingQueue2.take();
+				System.out.println(queueElement);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	
