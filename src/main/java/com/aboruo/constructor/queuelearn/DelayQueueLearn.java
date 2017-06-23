17823e3c1f76ac9118e28638f8d652342b4eca60
Aboruo <zhaoxinbo@cm-inv.com>
2017-06-23 20:42:45 +0800

    target目录无效变更

--- src/main/java/com/aboruo/constructor/queuelearn/DelayQueueLearn.java
+++ src/main/java/com/aboruo/constructor/queuelearn/DelayQueueLearn.java
@@ -1,15 +1,11 @@
 package com.aboruo.constructor.queuelearn;
 
 public class DelayQueueLearn implements Runnable {
-
 	@Override
 	public void run() {
-
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
